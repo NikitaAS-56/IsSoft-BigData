ec4c444a358f3301b7f4cd86dec8e8363be2a7b9
Nikita <Nekitos15102001@gmail.com>
2021-08-16 10:43:04 +0300

    Исправлены проблемы с сортировкой

--- top_film_task3_MyQSL/sql/usp_find_top_rated_movies.sql
+++ top_film_task3_MyQSL/sql/usp_find_top_rated_movies.sql
@@ -1,37 +1,41 @@
 Delimiter $$
-CREATE  PROCEDURE usp_find_top_rated_movies(
-    IN n INT,
-    IN `regexp` VARCHAR(200),
-    IN year_from INT,
-    IN year_to INT,
-    IN genres VARCHAR(256)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_find_top_rated_movies`(
+     IN n INT,
+IN `regexp` VARCHAR(200),
+IN year_from INT,
+IN year_to INT,
+IN genres VARCHAR(256)
 )
 BEGIN
+if genres ='NULL' then set genres =NULL;
+end if;
+with cte_1 as(
+        with cte as (
+        SELECT * FROM view_ratings)
+SELECT
+		movieId,
+		title,
+		mn.genre,
+		movieYear,
+		rating,
+		t.genres
+	from cte as t
 
-        with cte_1 as(
-            with cte as (
-                    SELECT * FROM view_ratings )
-            SELECT
-                   movieId,
-                   title,
-                   mn.genre,
-                   movieYear,
-                   rating,
-                   t.genres
-            from cte as t
+join json_table (
+		replace (json_array(t.genres), '|', '","'),
+		'$[*]' columns (genre varchar(50) path '$')
+) as mn
 
-            join json_table (
-                replace (json_array(t.genres),'|','","'),
-                '$[*]' columns (genre varchar(50) path '$')
-            ) as mn
-            )
-        SELECT movieId, title , movieYear, rating, m.genres from  cte_1 as m
+WHERE
+				((year_from IS NULL) OR (t.movieYear >= year_from))
+				AND ((year_to IS NULL) OR (t.movieYear <= year_to))
+				AND ((`regexp` IS NULL) OR (REGEXP_SUBSTR(t.title, `regexp`) != ''))
 
-        WHERE
-            ((year_from IS NULL) OR (m.movieYear >= year_from))
-            AND ((year_to IS NULL) OR (m.movieYear <= year_to))
-            AND ((`regexp` IS NULL) OR (REGEXP_SUBSTR(m.title,`regexp`) != ''))
-            AND REGEXP_SUBSTR(m.genre, genres) != ''
-        ORDER BY m.rating DESC, m.movieYear DESC LIMIT n;
+)
+SELECT distinct movieId, title, movieYear, rating, m.genres
+from cte_1 as m
+
+WHERE(genres is null) or REGEXP_SUBSTR( m.genre,genres) != ''
+ORDER BY m.rating DESC, m.movieYear DESC, m.title ASC LIMIT n;
 END$$
 Delimiter ;
